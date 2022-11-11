94d2bd1b6b7b3560803214d80223aab148cad822
Github <Github>
2022-11-11 21:25:02 +0900

    요소 존재 배열의 프로토타입 매칭

---
+++ Ch7/7-4.js
@@ -0,0 +1,14 @@
+var Grade = function () {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i = 0; i < args.length; i++) {
+        this[i] = args[i];
+    }
+    this.length = args.length;
+};
+Grade.prototype = ['a', 'b', 'c', 'd'];
+var g = new Grade (100, 80);
+g.push(90);
+console.log(g);  // Grade {0: 100, 1: 80, 2: 90, length: 3}
+delete g.length;
+g.push(70);
+console.log(g);  // Grade {0: 100, 1: 80, 2: 90, ___4: 70, length: 5 }
