1a16358fd4f1a3bb5df37cc06573257509bd4be7
Github <Github>
2022-11-11 20:47:04 +0900

    call/apply 메서드 활용

---
+++ Ch3/3-17.js
@@ -0,0 +1,12 @@
+var obj = {
+    0: 'a',
+    1: 'b',
+    2: 'c', 
+    length: 3
+};
+Array.prototype.push.call(obj, 'd');
+console.log(obj);	// { 0: 'a', 1: 'b', 2: 'c', 3: 'd', length: 4 }
+
+var arr = Array.prototype.slice.call(obj);
+console.log(arr);	// [ 'a', 'b', 'c', 'd'	]
+    
