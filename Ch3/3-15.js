bcb75ec7406d6adf4221082db73a3b87c485003d
Github <Github>
2022-11-11 20:46:15 +0900

    call 메서드

---
+++ Ch3/3-15.js
@@ -0,0 +1,10 @@
+var obj = {
+    a: 1, 
+    method: function (x, y) {
+        console.log(this.a, x, y);
+    
+    }
+};
+
+obj.method(2, 3);      // 1 2 3
+obj.method.call({ a: 4 }, 5, 6);  // 4 5 6
