96fbd09946c41bfb8997e51b8344f221d2358dfc
Github <Github>
2022-11-11 20:44:21 +0900

    화살표 함수

---
+++ Ch3/3-11.js
@@ -0,0 +1,12 @@
+var obj = {
+    outer: function () { 
+        console.log(this);        // (1) ( outer: f ) 
+        var innerFunc = () => { 
+            console.log(this);    // (2) ( outer: f )
+        };
+        innerFunc ();
+    }
+ };
+ obj.outer();	
+    
+    
