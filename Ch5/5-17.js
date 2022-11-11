1583762e0d0ebd0c078b8f1cffdc880f7c7ce07a
Github <Github>
2022-11-11 21:15:40 +0900

    커링함수

---
+++ Ch5/5-17.js
@@ -0,0 +1,16 @@
+var curry3 = function (func) { 
+    return function (a) {
+        return function (b) { 
+            return func(a, b);
+        };
+    };
+};
+
+var getMaxWith10 = curry3(Math.max)(10);
+console.log(getMaxWith10(8));	// 10
+console.log(getMaxWithlO(25));	// 25
+
+var getMinWith10 = curry3(Math.min)(10);
+console.log(getMinWith10(8));
+console.log(getMinWith10(24));
+    
