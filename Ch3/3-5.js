e1ddaa68e6a04f521160adf863255aa633c2aad7
Github <Github>
2022-11-11 20:41:21 +0900

    전역변수, 전역객체

---
+++ Ch3/3-5.js
@@ -0,0 +1,12 @@
+var a = 1;
+delete window.a;	// false
+console.log(a, window.a, this.a); // 1 1 1
+var b = 2;
+delete b;	// false
+console.log(b, window.b, this.b); // 2 2 2
+window.c = 3;
+delete window.c;	// true
+console.log(c, window.c, this.c); // Uncaught ReferenceError: c is not defined
+window.d = 4;
+delete d;
+console.log(d, window.d, this.d);
