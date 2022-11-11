58a2e675849d75f28ca904d3a8f7f4cb4abdc215
Github <Github>
2022-11-11 20:16:03 +0900

    undefined와 배열

---
+++ ch1/1-20.js
@@ -0,0 +1,9 @@
+var arrl = [];
+arr1.length = 3;
+console.log(arr1);	// [empty x 3]
+
+var arr2 = new Array(3);
+console.log(arr2);	// [empty x 3]
+
+var arr3 = [undefined, undefined, undefined];
+console.log(arr3);	// [undefined, undefined, undefined]
