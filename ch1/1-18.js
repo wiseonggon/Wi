820a864933f62177c0e25151f43bbb68a0150ca5
Github <Github>
2022-11-11 20:14:58 +0900

    JSON깊은복사

---
+++ ch1/1-18.js
@@ -0,0 +1,22 @@
+var copyObjectViaJSON = function (target) { 
+    return JSON.parse(JSON.stringify(target));
+};
+var obj = {
+a:	1,
+b:	{
+     c:	null,
+     d:	[1, 2],
+     func1: function () { console.log(3); }
+    },
+    func2: function () { console.log(4); }
+};
+var obj2 = copyObjectViaJSON(obj);
+
+obj2.a = 3;
+
+obj2.b.c = 4; 
+obj.b.d[1] = 3;
+    
+console.log(obj); // a: 1. b: ( c: null, d: (1, 3), fund: f() ), func2: f() ) 
+console.log(obj2); // I a: 3. b: ( c: 4,	d: [1, 21) 1
+    
